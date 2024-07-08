535066391b6059caee439c2238f1b7a8189f53b0
Saad Khawaja <158464977+saadnkhawaja@users.noreply.github.com>
2024-07-08 06:03:27 +0500

    Update InstantScreenshot.cs

--- InstantScreenshot.cs
+++ InstantScreenshot.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class InstantScreenshot : EditorWindow
 {
-    private int resWidth = Screen.width * 4;
-    private int resHeight = Screen.height * 4;
-    private Camera screenshotCamera;
-    private int scale = 1;
-    private string savePath = "";
-    private bool showPreview = true;
-    private RenderTexture renderTexture;
-    private bool isTransparent = false;
-    private bool takeHighResShot = false;
-    private string lastScreenshot = "";
+    private int resWidth = Screen.width * 4; // Default resolution width
+    private int resHeight = Screen.height * 4; // Default resolution height
+    private Camera screenshotCamera; // Camera to take the screenshot from
+    private int scale = 1; // Scale factor for the screenshot
+    private string savePath = ""; // Path to save the screenshot
+    private bool isTransparent = false; // Whether the screenshot background is transparent
+    private bool isTakingScreenshot = false; // Flag to indicate if a screenshot is being taken
+    private string lastScreenshot = ""; // Path of the last screenshot taken
+    private bool openFileAfterScreenshot = false; // Flag to indicate if the last screenshot should be opened after taking
 
     [MenuItem("Tools/Saad Khawaja/Instant High-res Screenshot")]
     public static void ShowWindow()
     {
+        // Show existing window instance or create a new one
         EditorWindow editorWindow = GetWindow<InstantScreenshot>();
         editorWindow.autoRepaintOnSceneChange = true;
         editorWindow.Show();
-        editorWindow.titleContent = new GUIContent("Screenshot");
+        editorWindow.titleContent = new GUIContent("Instant Screenshot");
     }
 
-    private Vector2 scrollPosition;
+    [MenuItem("Tools/Saad Khawaja/Take Screenshot %&t")] // Shortcut: Ctrl + Shift + T
+    public static void TakeScreenshotShortcut()
+    {
+        // Trigger screenshot through shortcut
+        InstantScreenshot window = GetWindow<InstantScreenshot>();
+        window.TakeHighResShot();
+    }
+
+    private Vector2 scrollPosition; // Scroll position for the scroll view
     private void OnGUI()
     {
         // Add padding to the window
@@ -48,6 +56,7 @@ public class InstantScreenshot : EditorWindow
         // Draw the label at the start
         GUILayout.Label("Instant Screenshot", titleStyle);
 
+        // Draw different sections of the UI
         DrawResolutionSettings();
         GUILayout.Space(10);
         DrawSavePathSettings();
@@ -108,11 +117,13 @@ public class InstantScreenshot : EditorWindow
         EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button("Set To Screen Size", GUILayout.Height(30)))
         {
+            // Set resolution to current screen size
             resWidth = (int)Handles.GetMainGameViewSize().x;
             resHeight = (int)Handles.GetMainGameViewSize().y;
         }
         if (GUILayout.Button("Default Size", GUILayout.Height(30)))
         {
+            // Set resolution to default size
             resWidth = 2560;
             resHeight = 1440;
             scale = 1;
@@ -128,12 +139,14 @@ public class InstantScreenshot : EditorWindow
         {
             if (string.IsNullOrEmpty(savePath))
             {
+                // If save path is not set, prompt user to set it
                 savePath = EditorUtility.SaveFolderPanel("Path to Save Images", savePath, Application.dataPath);
                 Debug.Log("Path Set");
                 TakeHighResShot();
             }
             else
             {
+                // Take the screenshot
                 TakeHighResShot();
             }
         }
@@ -143,25 +156,32 @@ public class InstantScreenshot : EditorWindow
 
     private void DrawActionButtons()
     {
+        openFileAfterScreenshot = EditorGUILayout.Toggle("Open Last File", openFileAfterScreenshot);
+        EditorGUILayout.Space();
+
         EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button("Open Last Screenshot", GUILayout.MaxWidth(160), GUILayout.MinHeight(40)))
         {
             if (!string.IsNullOrEmpty(lastScreenshot))
             {
+                // Open the last screenshot taken
                 Application.OpenURL("file://" + lastScreenshot);
                 Debug.Log("Opening File " + lastScreenshot);
             }
         }
         if (GUILayout.Button("Open Folder", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
         {
+            // Open the folder containing the screenshots
             Application.OpenURL("file://" + savePath);
         }
         if (GUILayout.Button("More Assets", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
         {
+            // Open the asset store page
             Application.OpenURL("https://assetstore.unity.com/publishers/5951");
         }
         if (GUILayout.Button("Github", GUILayout.MaxWidth(100), GUILayout.MinHeight(40)))
         {
+            // Open the GitHub page
             Application.OpenURL("https://github.com/saadnkhawaja/instant-screenshot-unity");
         }
         EditorGUILayout.EndHorizontal();
@@ -169,6 +189,7 @@ public class InstantScreenshot : EditorWindow
 
     private string GenerateScreenshotName(int width, int height)
     {
+        // Generate a unique filename for the screenshot
         string fileName = string.Format("{0}/screen_{1}x{2}_{3}.png", savePath, width, height, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
         lastScreenshot = fileName;
         return fileName;
@@ -177,18 +198,20 @@ public class InstantScreenshot : EditorWindow
     public void TakeHighResShot()
     {
         Debug.Log("Taking Screenshot");
-        takeHighResShot = true;
+        isTakingScreenshot = true;
     }
 
     private void Update()
     {
-        if (takeHighResShot)
+        if (isTakingScreenshot)
         {
+            // Calculate resolution
             int resWidthN = resWidth * scale;
             int resHeightN = resHeight * scale;
             RenderTexture rt = new RenderTexture(resWidthN, resHeightN, 24);
             screenshotCamera.targetTexture = rt;
 
+            // Set texture format based on transparency option
             TextureFormat tFormat = isTransparent ? TextureFormat.ARGB32 : TextureFormat.RGB24;
             Texture2D screenShot = new Texture2D(resWidthN, resHeightN, tFormat, false);
             screenshotCamera.Render();
@@ -196,11 +219,21 @@ public class InstantScreenshot : EditorWindow
             screenShot.ReadPixels(new Rect(0, 0, resWidthN, resHeightN), 0, 0);
             screenshotCamera.targetTexture = null;
             RenderTexture.active = null;
+
+            // Save the screenshot
             byte[] bytes = screenShot.EncodeToPNG();
             string fileName = GenerateScreenshotName(resWidthN, resHeightN);
             System.IO.File.WriteAllBytes(fileName, bytes);
             Debug.Log($"Took screenshot to: {fileName}");
-            takeHighResShot = false;
+            ShowNotification(new GUIContent("Screenshot taken!"));
+
+            // Open the file if the option is selected
+            if (openFileAfterScreenshot)
+            {
+                Application.OpenURL("file://" + fileName);
+            }
+
+            isTakingScreenshot = false;
         }
     }
 }
